4d78788da64e737c7231d0ad4538c2078b78802d
Emily Jewik <emilyjewik@gmail.com>
2018-09-22 11:33:23 -0700

    Initial commit will configure audio to text base functionality

---
+++ SpeechText/TextToSpeech.swift
@@ -0,0 +1,16 @@
+//
+//  TextToSpeech.swift
+//  SpeechText
+//
+//  Created by Emily Jewik on 9/22/18.
+//  Copyright © 2018 Emily Jewik. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class TextToSpeech : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
